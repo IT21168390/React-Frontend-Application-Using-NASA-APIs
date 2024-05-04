a1ded710f4aed8ba02d41d37650157ff9ccedba6
Hashan Madhuwantha <99306792+IT21168390@users.noreply.github.com>
2024-05-04 21:34:18 +0530

    Update Map.jsx zoom level

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -100,7 +100,7 @@ function Map({ eventsData, center, zoom }) {
 
     const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
+        //map.fitBounds(bounds);
 
         setMap(map)
     }, [])
@@ -120,7 +120,7 @@ function Map({ eventsData, center, zoom }) {
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={5}
+            zoom={zoom}
             onLoad={onLoad}
             onUnmount={onUnmount}
         >
